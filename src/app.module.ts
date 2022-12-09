666c0a488bb2851c0368e7b9c080172c67029e82
MercedesLaFuente <mercedes.htcv@gmail.com>
2022-12-09 12:50:01 -0400

    funcional DB

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from './config/constants';
+
+@Module({
+  imports: [ConfigModule.forRoot({ 
+    envFilePath: '.env', 
+    isGlobal: true 
+  }),
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: (configService: ConfigService) => ({
+      type: 'mariadb',
+      host: configService.get<string>(DB_HOST),
+      port: +configService.get<number>(DB_PORT),
+      username: configService.get<string>(DB_USER),
+      password: configService.get<string>(DB_PASSWORD),
+      database: configService.get<string>(DB_DATABASE),
+      entities: [],
+      synchronize: true,
+    }),
+    inject: [ConfigService],
+  })
+],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
