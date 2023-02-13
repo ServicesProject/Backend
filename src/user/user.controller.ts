23fec04b84278c00ca8dc1a33420671d919b0903
MercedesLaFuente <mercedes.htcv@gmail.com>
2023-02-13 18:01:54 -0400

    last change

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
-import { Authorization } from 'src/decorators/authorization.decorator';
+//import { Authorization } from 'src/decorators/authorization.decorator';
 import { UserDto } from './dto/user.dto';
 import { UserService } from './user.service';
 
