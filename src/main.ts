42fda8671eb98ff02548a532767f5f5bf0b5580c
MercedesLaFuente <mercedes.htcv@gmail.com>
2023-05-13 02:02:35 -0400

    change port x2

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get(ConfigService);
+  
+  console.log('********************');
+
   const serverPort: number = parseInt(process.env.SERVER_PORT, 10);
   const port: number = configService.get<number>('SERVER_PORT', serverPort);
-
+  console.log('PORT: ',port);
+  console.log('********************');
 
   await app.listen(port);
 }
