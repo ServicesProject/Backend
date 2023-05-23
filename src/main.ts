c6cb0f9ba2d5d10d483948510a73a23d0603079e
MercedesLaFuente <mercedes.htcv@gmail.com>
2023-05-23 18:36:34 -0400

    trying deploy

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,7 @@ import { AppModule } from './app.module';
 import { SERVER_PORT } from './config/constants';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  const configService = app.get(ConfigService);
-  //server port
-  const port = +configService.get<number>(SERVER_PORT) || 3000;
-  await app.listen(port);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.SERVER_PORT || 3000);
 }
 bootstrap();
