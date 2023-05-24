21af7ce6672d5af83f36ff3d4ae8d24ba16f8a0a
MercedesLaFuente <mercedes.htcv@gmail.com>
2023-05-23 22:39:01 -0400

    trying deploy

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { SERVER_PORT } from './config/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.SERVER_PORT || 3000);
 }
 bootstrap();
