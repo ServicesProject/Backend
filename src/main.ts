11fda4ec2be8ab210cc1ffb4c1c52a24192f16c9
MercedesLaFuente <mercedes.htcv@gmail.com>
2023-05-23 23:54:32 -0400

    trying deploy

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { ConfigService } from '@nestjs/config/dist';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from './config/constants';
 import { SERVER_PORT } from './config/constants';
 
 async function bootstrap() {
+  console.log('***********************************');
+  console.log(process.env.SERVER_PORT);
+  console.log(DB_HOST)
+  console.log('***********************************');
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.SERVER_PORT || 3000);
+
 }
 bootstrap();
