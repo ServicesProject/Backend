28b5fde721e858edf38a57038a4ddcaec2e4d7a3
MercedesLaFuente <mercedes.htcv@gmail.com>
2023-05-24 00:10:06 -0400

    trying deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -59,7 +59,8 @@ import { NotificationModule } from './notification/notification.module';
     useFactory: (configService: ConfigService) => ({
       type: 'mariadb',
       host: configService.get<string>(DB_HOST),
-      port: +configService.get<number>(DB_PORT),
+      
+      port: configService.get<number>(DB_PORT),
       username: configService.get<string>(DB_USER),
       password: configService.get<string>(DB_PASSWORD),
       database: configService.get<string>(DB_DATABASE),
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import { ConfigService } from '@nestjs/config/dist';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from './config/constants';
-import { SERVER_PORT } from './config/constants';
 
 async function bootstrap() {
   console.log('***********************************');
   console.log(process.env.SERVER_PORT);
-  console.log(DB_HOST)
+  console.log(process.env.DB_HOST);
+  console.log(process.env.DB_DATABASE);
+  console.log(process.env.DB_PORT);
+  console.log(process.env.DB_USER);
+
   console.log('***********************************');
   const app = await NestFactory.create(AppModule);
   app.enableCors();
