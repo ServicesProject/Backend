17348ee232fe37f96f7b7d4f67704dc98ac76448
MercedesLaFuente <mercedes.htcv@gmail.com>
2023-05-24 03:04:57 -0400

    fixed mailer

--- src/rating/rating.controller.ts
+++ src/rating/rating.controller.ts
@@ -5,14 +5,11 @@ import { RatingDto } from './dto/rating.dto';
 
 @Controller('rating')
 export class RatingController {
-
     constructor(private readonly ratingService: RatingService){}
-    
     @Post()
     async createRating(@Body() dto: RatingDto){
         return await this.ratingService.createRating(dto)
     }
-
     @Get(':id/average-points')
     async getAveragePointsForWork(@Param('id') id: number) {
       const averagePoints = await this.ratingService.averagePointsForWork(id);
